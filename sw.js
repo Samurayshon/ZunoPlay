8c662cf8d0087635168f64a8c4045501e59e26ac
ZunoPlay <giulrocha13@gmail.com>
2026-08-26 01:10:30 -0300

    Update service worker to version v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "zunoplay-v1";
+const CACHE_NAME = "zunoplay-v2";
 
 const FILES_TO_CACHE = [
     "./",
@@ -6,63 +6,134 @@ const FILES_TO_CACHE = [
     "./manifest.json"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener(
+    "install",
+    event => {
 
-    event.waitUntil(
+        event.waitUntil(
 
-        caches.open(CACHE_NAME)
-            .then(cache => {
+            caches
+                .open(CACHE_NAME)
+                .then(
+                    cache => {
 
-                return cache.addAll(
-                    FILES_TO_CACHE
-                );
+                        return cache.addAll(
+                            FILES_TO_CACHE
+                        );
 
-            })
+                    }
+                )
 
-    );
+        );
 
-    self.skipWaiting();
+        self.skipWaiting();
 
-});
-
-
-self.addEventListener("activate", event => {
-
-    event.waitUntil(
-
-        caches.keys()
-            .then(keys => {
-
-                return Promise.all(
-
-                    keys
-                        .filter(key => key !== CACHE_NAME)
-                        .map(key => caches.delete(key))
-
-                );
-
-            })
-
-    );
-
-    self.clients.claim();
-
-});
+    }
+);
 
 
-self.addEventListener("fetch", event => {
+self.addEventListener(
+    "activate",
+    event => {
 
-    event.respondWith(
+        event.waitUntil(
 
-        fetch(event.request)
-            .catch(() => {
+            caches
+                .keys()
+                .then(
+                    keys => {
 
-                return caches.match(
-                    event.request
-                );
+                        return Promise.all(
 
-            })
+                            keys
+                                .filter(
+                                    key =>
+                                        key !== CACHE_NAME
+                                )
+                                .map(
+                                    key =>
+                                        caches.delete(key)
+                                )
 
-    );
+                        );
 
-});
+                    }
+                )
+
+        );
+
+        self.clients.claim();
+
+    }
+);
+
+
+self.addEventListener(
+    "fetch",
+    event => {
+
+        /*
+         * Primeiro tenta buscar
+         * a versão atual da internet.
+         */
+
+        event.respondWith(
+
+            fetch(
+                event.request
+            )
+            .then(
+                response => {
+
+                    /*
+                     * Guarda uma cópia
+                     * quando possível.
+                     */
+
+                    if(
+                        response &&
+                        response.status === 200 &&
+                        event.request.method === "GET"
+                    ){
+
+                        const responseClone =
+                            response.clone();
+
+                        caches
+                            .open(CACHE_NAME)
+                            .then(
+                                cache => {
+
+                                    cache.put(
+                                        event.request,
+                                        responseClone
+                                    );
+
+                                }
+                            );
+
+                    }
+
+                    return response;
+
+                }
+            )
+            .catch(
+                () => {
+
+                    /*
+                     * Se estiver offline,
+                     * tenta usar o cache.
+                     */
+
+                    return caches.match(
+                        event.request
+                    );
+
+                }
+            )
+
+        );
+
+    }
+);
