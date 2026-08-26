737cea42f0e306916b3b62cdda6fda645608abed
ZunoPlay <giulrocha13@gmail.com>
2026-08-26 16:57:25 -0300

    Bump PWA cache after stabilization fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "zunoplay-v12";
+const CACHE_NAME = "zunoplay-v13";
 const STATIC_FILES = ["./", "./index.html", "./manifest.json", "./nav.js", "./realtime-global.js", "./presenca-sala.js"];
 
 self.addEventListener("install", event => {
