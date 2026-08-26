a3c88556779e25d7153b6b72334a5059d539fe46
ZunoPlay <giulrocha13@gmail.com>
2026-08-26 16:29:07 -0300

    Refresh PWA cache for private presence

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "zunoplay-v11";
+const CACHE_NAME = "zunoplay-v12";
 const STATIC_FILES = ["./", "./index.html", "./manifest.json", "./nav.js", "./realtime-global.js", "./presenca-sala.js"];
 
 self.addEventListener("install", event => {
