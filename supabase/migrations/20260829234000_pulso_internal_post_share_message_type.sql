ef2cc39ecd7f0364623572aa2431dee436ee0a8f
ZunoPlay <giulrocha13@gmail.com>
2026-08-29 18:19:38 -0300

    db: allow internal Pulso post share messages

---
+++ supabase/migrations/20260829234000_pulso_internal_post_share_message_type.sql
@@ -0,0 +1,12 @@
+alter table public.messages
+  drop constraint if exists messages_type_chk;
+
+alter table public.messages
+  add constraint messages_type_chk
+  check (
+    type = any (
+      array[
+        'text','image','video','audio','file','gif','sticker','system','post_share'
+      ]::text[]
+    )
+  );
